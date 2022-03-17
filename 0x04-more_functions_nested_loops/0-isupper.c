50be5dd0a42e2ebfe9ee364393221681f977aaae
ekow26 <93169004+ekow26@users.noreply.github.com>
2022-03-17 21:00:44 +0000

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
+#include <ctype.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _isupper - function block
+ * Description: check for uppercase
+ * @c: integer
+ * Return: 0
  */
-int main(void)
-{
-	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+int _isupper(int c)
+{
+int a = 0;
+
+if (c >= 65 && c <= 90)
+a = 1;
+return (a);
 }
