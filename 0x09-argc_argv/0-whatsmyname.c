35c8cda52a49af707bb103bc9316c6b1b56f0a53
ekow26 <93169004+ekow26@users.noreply.github.com>
2022-04-01 22:46:01 +0000

    Update 0-whatsmyname.c

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - a program that prints its name
+ * @argc: the argument count
+ * @argv: argument values or a pointer to an array of strings
  *
- * Return: Always 0 (Success)
+ * Return: the name of the program
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
